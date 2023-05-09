1983638a46e920a7863483cdf92f0292e7b8ce2a
GShashikanth <83594925+GShashikanth@users.noreply.github.com>
2023-05-09 19:35:02 +0530

    Add files via upload

---
+++ Push_codes/rest_sample__cl/rest_sample__cl.ino
@@ -0,0 +1,68 @@
+/***Abhro***/
+// Control ESP8266 anywhere
+
+// Import required libraries
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <aREST.h>
+
+// Clients
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+// Create aREST instance
+aREST rest = aREST(client);
+
+// Unique ID to identify the device for cloud.arest.io
+const char* device_id = "a82023";
+
+// WiFi parameters
+const char* ssid = "AJAI_Testing";
+const char* password = "Adso@4321";
+
+
+const char* out_topic;
+
+
+// Functions
+void callback(char* topic, byte* payload, unsigned int length);
+
+void setup(void)
+{
+  // Start Serial
+  Serial.begin(115200);
+
+  // Set callback
+  client.setCallback(callback);
+  
+  // Give name and ID to device
+  rest.set_id(device_id);
+  rest.set_name("Emergency Stop cloud");
+
+  // Connect to WiFi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+
+  // Set output topic
+//  out_topic = rest.ge/t_topic();
+
+}
+
+void loop() {
+
+  // Connect to the cloud
+  rest.loop(client);
+
+}
+
+// Handles message arrived on subscribed topic(s)
+void callback(char* topic, byte* payload, unsigned int length) {
+
+  rest.handle_callback(client, topic, payload, length);
+
+}
---
+++ Push_codes/url_trig.py
@@ -0,0 +1,24 @@
+import requests
+import time
+from tkinter import *
+
+session = requests.Session()
+session.trust_env = False
+# response = session.get("https://cloud.arest.io/a82023/digital/2/1") #optional 
+def on():
+    response = session.get("https://cloud.arest.io/a82023/digital/2/0") #
+def off():
+    response = session.get("https://cloud.arest.io/a82023/digital/2/1")
+
+
+window= Tk()
+window.geometry("500x200")
+window.title("Emergency stop window")
+
+B1 = Button(window, text="STOP", width=20, bg="red", fg="black", command=on)
+B1.place(x=50,y=90)
+
+B2 = Button(window, text="GET READY", width=20, bg="green", fg="black", command=off)
+B2.place(x=250,y=90)
+
+window.mainloop()
